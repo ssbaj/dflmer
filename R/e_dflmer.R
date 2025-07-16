cf209cf6e4cea0a4bb1e4b4fc57ae2203f2472aa
ssbaj <ssb9018@ajou.ac.kr>
2025-07-16 10:03:56 +0900

    first commit

---
+++ R/e_dflmer.R
@@ -0,0 +1,11 @@
+e_dflmer<-function(explaining=0){
+
+if(explaining==0) {
+cat("  ", '\n')
+cat("\033[1;32m## lmer dataset ----------- \033[0m", '\n')
+cat("  HLM 완벽 가이드 dataset ", '\n')
+cat("  1) salary_data ", '\n')
+cat("  2) therapie ", '\n')
+cat("   ", '\n')
+} }
+
